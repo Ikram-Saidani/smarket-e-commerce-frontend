857c43b2efb94b1b91966074599114e48851505b
Ikram Saidani <saidaniikram26@gmail.com>
2024-12-14 00:29:58 +0100

    modification of price toFixed

--- src/Components/Orders/ProductPassedBox.jsx
+++ src/Components/Orders/ProductPassedBox.jsx
@@ -12,7 +12,7 @@ function ProductPassedBox({ item }) {
       <td className="title">{item.productId?.title?.length>40?item.productId?.title.slice(0,40)+"...":item.productId?.title}</td>
       <td className="price">{item.productId?.price.toFixed(2)} TND</td>
       <td className="qantity">{item.quantity}</td>
-      <td className="totalPrice">{item.totalPrice} TND</td>
+      <td className="totalPrice">{item.totalPrice.toFixed(2)} TND</td>
     </tr>
   );
 }
